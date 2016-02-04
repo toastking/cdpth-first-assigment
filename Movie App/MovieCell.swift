1a256dc0666b98353ac2ed8881e004e2ac7bfe66
Matt Del Signore <mattdelsig@me.com>
2016-02-03 23:05:11 -0500

    added movie poster images

--- Movie App/MovieCell.swift
+++ Movie App/MovieCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MovieCell: UITableViewCell {
 
+    @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var overviewLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     override func awakeFromNib() {
--- Movie App/MoviesViewController.swift
+++ Movie App/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -76,9 +77,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
+        let baseUrl = "http://image.tmdb.org/t/p/w500/"
+        
+        let imgUrl = NSURL(string: baseUrl + posterPath)
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        cell.posterView.setImageWithURL(imgUrl!)
         
         //print the value to the console
         print("row  \(indexPath.row)")
